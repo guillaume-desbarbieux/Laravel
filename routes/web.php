aab77fb4f887214d1354ea679873f67d21686b49
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-07-22 13:45:02 +0200

    Managing URL in route

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,26 @@ Route::get('/', function () {
 /*/
 
 
+Route::get('/', function () {
+    return view('HelloWorld');
+});
+
 
 Route::get('/', function () {
-$laravel = app()->version();
-$php = phpversion();
-    return ("HelloWorld from Laravel v$laravel (PHP v$php)");
+    $laravel = app()->version();
+    $php = phpversion();
+    return "HelloWorld from Laravel v$laravel (PHP v$php)";
+});
+
+
+Route::get('/product', function () {
+     return "Liste des produits";
+});
+
+Route::get('/product/{id}', function (string $id) {
+    return "Fiche du produit ".$id;
+});
+
+Route::get('/cart', function () {
+    return "Panier";
 });
