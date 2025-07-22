30b2e36555ef98d0cb104eb48cbdbf5e76027a4c
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-07-22 12:21:26 +0200

    Hello world with version

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*/
 Route::get('/', function () {
     return view('welcome');
 });
+/*/
+
+
 
 Route::get('/', function () {
-    return view('HelloWorld');
+$laravel = app()->version();
+$php = phpversion();
+    return ("HelloWorld from Laravel v$laravel (PHP v$php)");
 });
