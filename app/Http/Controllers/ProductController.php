65e6c2c26925a9195e5cc258c4668273b5e8f6f0
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-07-22 14:21:32 +0200

    Create Controllers

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index() {
+        return "Liste de Produits";
+    }
+
+    public function show($id) {
+        return "Fiche du produit $id";
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*/
-Route::get('/', function () {
-    return view('welcome');
-});
-/*/
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
+//Route::get('/', function () {
+//    return view('HelloWorld');
+//});
 
-Route::get('/', function () {
-    return view('HelloWorld');
-});
+//Route::get('/', function () {
+//    $laravel = app()->version();
+//    $php = phpversion();
+//    return "HelloWorld from Laravel v$laravel (PHP v$php)";
+//});
 
+use App\Http\Controllers\HomeController;
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/', function () {
-    $laravel = app()->version();
-    $php = phpversion();
-    return "HelloWorld from Laravel v$laravel (PHP v$php)";
-});
+use App\Http\Controllers\ProductController;
+Route::get('/product', [ProductController::class, 'index']);
+Route::get('/product/{id}', [ProductController::class, 'show']);
 
-
-Route::get('/product', function () {
-     return "Liste des produits";
-});
-
-Route::get('/product/{id}', function (string $id) {
-    return "Fiche du produit ".$id;
-});
-
-Route::get('/cart', function () {
-    return "Panier";
-});
+use App\Http\Controllers\CartController;
+Route::get('/cart', [CartController::class, 'index']);
